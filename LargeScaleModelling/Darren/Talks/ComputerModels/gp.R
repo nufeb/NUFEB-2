b4a4cf9678fc98ff1e35ddcf1fd6bdc47339bd91
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2014-05-28 22:53:57 +0100

    working on talk

--- LargeScaleModelling/Darren/Talks/ComputerModels/gp.R
+++ LargeScaleModelling/Darren/Talks/ComputerModels/gp.R
@@ -3,13 +3,16 @@
 
 # setup
 Grid=seq(0,1,0.01)
-K=function(d,scale=20,lengthScale=0.4)
+
+# GP covariance kernel
+K=function(d,scale=50,lengthScale=0.4)
 {
   (scale^2)*exp(-(d/lengthScale)^2)
 }
 
-# function definitions
-GPAdjust=function(Grid,ObsLoc,Obs) {
+# fit a mean zero GP
+GPAdjust=function(Grid,ObsLoc,Obs) 
+{
   lG=length(Grid)
   lO=length(ObsLoc)
   All=c(Grid,ObsLoc)
@@ -22,29 +25,60 @@ GPAdjust=function(Grid,ObsLoc,Obs) {
   ViC=solve(VO,CovOG)
   AdjEx=t(ViC)%*%Obs
   AdjVar=VG-CovGO%*%ViC
-  list(Ex=as.vector(AdjEx),Var=diag(AdjVar))
+  AdjVar=AdjVar+diag(rep((1e-6)*max(AdjVar),lG)) # numerical stability fudge
+  list(Ex=as.vector(AdjEx),Var=AdjVar)
 }
 
+# fit a GP to the residuals from a linear regression fit
+GPAdjustLR=function(Grid,ObsLoc,Obs) 
+{
+  Mod=lm(Obs~ObsLoc)
+  Pred=predict(Mod,newdata=data.frame(ObsLoc=Grid))
+  Raw=GPAdjust(Grid,ObsLoc,Mod$residuals)
+  list(Ex=Raw$Ex+Pred,Var=Raw$Var)
+}
+
+# produce a plot illustrating the GP fit
 plotAdjust=function(Grid,ObsLoc,Obs,...)
 {
   plot(ObsLoc,Obs,xlim=range(Grid),pch=19,...)
-  Adj=GPAdjust(Grid,ObsLoc,Obs)
-  Adj$Var[Adj$Var<0]=0 # numerical stability fudge
-  Upper=Adj$Ex+2*sqrt(Adj$Var)
-  Lower=Adj$Ex-2*sqrt(Adj$Var)
+  Adj=GPAdjustLR(Grid,ObsLoc,Obs)
+  Var=diag(Adj$Var)
+  Upper=Adj$Ex+2*sqrt(Var)
+  Lower=Adj$Ex-2*sqrt(Var)
   polygon(c(Grid,rev(Grid),Grid[1]),c(Upper,rev(Lower),Upper[1]),col="gray",border=NA)
   lines(Grid,Adj$Ex,lwd=2)
   points(ObsLoc,Obs,pch=19)
 }
 
-# calculations
-ObsLoc=c(0.3,0.6,0.4,0.9,0.1)
-Obs=c(10,11,8,12,12)
-for (i in 1:length(Obs)) {
-  plotAdjust(Grid,ObsLoc[1:i],Obs[1:i],ylim=c(0,20))
+# generate a sample from an adjusted object
+sampleGP=function(Adj)
+{
+  L=t(chol(Adj$Var))
+  Adj$Ex + L%*%rnorm(length(Adj$Ex))
 }
 
+# Now actually do some stuff...
+ObsLoc=c(0.3,0.6,0.4,0.9,0.1,0.8)
+Obs=c(10,11,8,12,12,13)
 
+# Show data progressively
+for (i in 1:length(Obs))
+  plot(ObsLoc[1:i],Obs[1:i],xlim=range(Grid),pch=19,ylim=c(0,20))
+
+# Show a progressive GP fit
+for (i in 1:length(Obs))
+  plotAdjust(Grid,ObsLoc[1:i],Obs[1:i],ylim=c(0,20))
+
+# Now show some samples from a GP
+
+for (i in 10^(0:2)) {
+  plot(ObsLoc,Obs,xlim=range(Grid),pch=19,ylim=c(0,20))
+  Adj=GPAdjustLR(Grid,ObsLoc,Obs)
+  for (j in 1:i)
+    lines(Grid,sampleGP(Adj),col="gray")
+  points(ObsLoc,Obs,pch=19)
+  }
 
 # eof
 
