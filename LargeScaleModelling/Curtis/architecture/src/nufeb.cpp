402829b08dec599e1c1141e87b12b3b8f5c29e9b
Curtis Madsen <curtis.madsen@newcastle.ac.uk>
2015-04-01 18:11:58 +0100

    Started working on code.

--- LargeScaleModelling/Curtis/architecture/src/nufeb.cpp
+++ LargeScaleModelling/Curtis/architecture/src/nufeb.cpp
@@ -1,4 +1,16 @@
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
 
-  return 0;
+	// Initialize model
+
+	double time = 0;  // Initialize time
+	double limit = 100; // Initialize time limit
+	double step = 1; // Initialize time step
+	while (time < limit) {
+		time += step;  // Advance time
+		// Update model
+		// Biological evolution
+		// Fluid phase model
+		// Particle based physics model
+	}
+	return 0;
 }
