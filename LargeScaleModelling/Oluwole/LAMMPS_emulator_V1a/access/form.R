b79cfe4df876a1225b95fc3387501bcdcfc2724d
Oluwole Oyebamiji <noo11@hardy.ncl.ac.uk>
2016-01-18 16:53:16 +0000

    Emulator version 1a

---
+++ LargeScaleModelling/Oluwole/LAMMPS_emulator_V1a/access/form.R
@@ -0,0 +1,115 @@
+c1=~KsHET + Ko2HET + Kno2HET + Kno3HET + Knh4AOB + Ko2AOB + Kno2NOB + 
+    Ko2NOB + MumHET + MumAOB + MumNOB + etaHET + bHET + bAOB + 
+    bNOB + bEPS + YEPS + YHET + EPSratio + factor + sub + no2 + 
+    no3 + o2 + nh4 + time + EPSratio:I(EPSratio * 1) + time:I(time * 
+    1) + YEPS:EPSratio + YHET:EPSratio + MumHET:bNOB + bHET:no3 + 
+    Kno2NOB:no2 + MumHET:bEPS + MumNOB:bAOB + Ko2NOB:no3 + MumHET:bAOB + 
+    KsHET:I(KsHET * 1) + Ko2HET:Ko2NOB + Kno2HET:o2 + Knh4AOB:etaHET + 
+    bHET:bEPS + Ko2NOB:factor + MumAOB:bHET + MumHET:MumNOB + 
+    bHET:YHET + MumAOB:YHET + Kno2HET:no3 + bEPS:YEPS + Kno3HET:o2 + 
+    KsHET:no3 + bAOB:YHET + Ko2NOB:bNOB + bAOB:bNOB + Kno3HET:bHET + 
+    Knh4AOB:bHET + YEPS:time + Kno2HET:MumNOB + MumHET:sub + 
+    Kno2NOB:bEPS + Ko2HET:nh4 + Kno2NOB:I(Kno2NOB * 1) + etaHET:bNOB + 
+    Kno2HET:Ko2AOB + YEPS:I(YEPS * 1) + bAOB:I(bAOB * 1) + Ko2HET:Kno2NOB + 
+    Ko2AOB:sub + MumHET:time + YEPS:no3 + Ko2NOB:sub + Kno2NOB:bHET + 
+    Ko2NOB:YHET + Kno3HET:bNOB + KsHET:bHET + EPSratio:o2 + Ko2NOB:no2 + 
+    Ko2AOB:YHET + bNOB:bEPS + Ko2NOB:nh4 + MumNOB:nh4 + Knh4AOB:I(Knh4AOB * 
+    1) + Knh4AOB:nh4 + Ko2AOB:MumNOB + bAOB:no2 + MumNOB:I(MumNOB * 
+    1) + Ko2AOB:MumAOB + Knh4AOB:bEPS + Ko2HET:Knh4AOB + Ko2HET:I(Ko2HET * 
+    1) + Ko2AOB:Kno2NOB + etaHET:I(etaHET * 1) + no2:nh4 + bNOB:no3 + 
+    KsHET:bNOB + bHET:o2 + Ko2AOB:o2 + Ko2HET:no2 + Ko2HET:Kno2HET + 
+    Ko2HET:etaHET + Kno2HET:etaHET + YHET:sub + Kno2NOB:YEPS + 
+    MumAOB:I(MumAOB * 1) + Ko2HET:no3 + Kno2HET:MumAOB + MumNOB:no3 + 
+    Kno3HET:etaHET + Ko2HET:MumAOB + Ko2HET:bHET + MumAOB:factor + 
+    Ko2NOB:bEPS + Kno3HET:no2 + bEPS:I(bEPS * 1) + EPSratio:time + 
+    bHET:no2 + o2:nh4 + Kno2NOB:EPSratio + factor:no2 + Knh4AOB:Kno2NOB + 
+    Kno2NOB:sub + Kno2HET:YHET + MumAOB:EPSratio + Ko2NOB:MumAOB + 
+    MumNOB:time + Kno3HET:MumNOB + KsHET:MumNOB + Kno2NOB:etaHET + 
+    KsHET:bAOB + KsHET:MumAOB + bNOB:no2 + Knh4AOB:YHET + Kno2HET:sub + 
+    EPSratio:nh4 + factor:no3 + KsHET:Ko2NOB + MumHET:no3 + Kno2HET:nh4 + 
+    Ko2NOB:etaHET + bNOB:EPSratio + etaHET:YHET + bEPS:o2 + Kno2NOB:MumHET + 
+    Kno2HET:Knh4AOB + Kno3HET:MumHET + YHET:no2 + Ko2AOB:time + 
+    Knh4AOB:factor + bNOB:o2 + Kno3HET:Knh4AOB + Kno2HET:MumHET + 
+    Knh4AOB:YEPS + Knh4AOB:no2 + MumHET:factor + Ko2HET:factor + 
+    Ko2HET:EPSratio + Ko2AOB:bAOB + MumNOB:etaHET + bEPS:factor + 
+    KsHET:nh4 + MumAOB:bEPS + MumAOB:bNOB + Knh4AOB:EPSratio + 
+    MumNOB:YHET + bEPS:no3 + MumHET:EPSratio + factor:time + 
+    bEPS:no2 + Kno3HET:I(Kno3HET * 1) + Knh4AOB:sub + factor:o2 + 
+    etaHET:EPSratio + Kno2HET:I(Kno2HET * 1) + KsHET:sub + YHET:nh4 + 
+    YHET:time + KsHET:no2 + bAOB:nh4 + MumHET:MumAOB + YHET:I(YHET * 
+    1) + Kno3HET:factor + factor:I(factor * 1) + Kno2HET:Kno2NOB + 
+    Ko2NOB:MumHET + bHET:EPSratio + bHET:nh4 + Ko2HET:bNOB + 
+    Ko2AOB:I(Ko2AOB * 1) + EPSratio:factor + bHET:bNOB + KsHET:Kno3HET + 
+    MumHET:nh4 + Kno2NOB:Ko2NOB + Knh4AOB:MumNOB + MumNOB:sub + 
+    Kno2HET:bNOB + Kno3HET:time + Ko2NOB:time + Kno3HET:Ko2AOB + 
+    KsHET:MumHET + sub:nh4 + bNOB:I(bNOB * 1) + KsHET:Kno2HET + 
+    Kno3HET:bAOB + MumHET:YEPS + Ko2AOB:no3 + no2:o2 + no2:I(no2 * 
+    1) + Ko2AOB:etaHET + MumNOB:factor + etaHET:factor + etaHET:nh4 + 
+    YEPS:nh4 + Kno2HET:Ko2NOB + etaHET:bEPS + Knh4AOB:Ko2NOB + 
+    Knh4AOB:Ko2AOB + MumHET:etaHET + no2:no3 + no3:I(no3 * 1) + 
+    etaHET:no2 + Ko2AOB:bEPS + YEPS:factor + KsHET:o2 + YHET:factor + 
+    bNOB:time + Ko2AOB:EPSratio + EPSratio:no3 + MumAOB:sub + 
+    Kno2NOB:no3 + Ko2AOB:YEPS + bAOB:EPSratio + Ko2NOB:YEPS + 
+    Kno2NOB:o2 + YEPS:no2 + MumHET:o2 + MumHET:bHET + Ko2HET:MumNOB + 
+    bAOB:sub + bAOB:time + no2:time + Knh4AOB:time + no3:time + 
+    KsHET:factor + MumAOB:MumNOB + MumAOB:o2 + Kno2HET:time + 
+    Kno2HET:YEPS + Knh4AOB:bNOB + Ko2AOB:bNOB + bEPS:sub + Kno3HET:EPSratio + 
+    etaHET:bHET + YHET:o2 + Kno2NOB:time + sub:I(sub * 1) + Kno3HET:YEPS + 
+    bNOB:YEPS + o2:time
+
+c2= ~KsHET + Ko2HET + Kno2HET + Kno3HET + Knh4AOB + Ko2AOB + Kno2NOB + 
+    Ko2NOB + MumHET + MumAOB + MumNOB + etaHET + bHET + bAOB + 
+    bNOB + bEPS + YEPS + YHET + EPSratio + factor + sub + no2 + 
+    no3 + o2 + nh4 + time + EPSratio:I(EPSratio * 1) + time:I(time * 
+    1) + YEPS:EPSratio + YHET:EPSratio + MumHET:bNOB + Kno3HET:Kno2NOB + 
+    bHET:no3 + Kno2NOB:no2 + Ko2NOB:I(Ko2NOB * 1) + Knh4AOB:etaHET + 
+    Ko2HET:Ko2NOB + Ko2AOB:MumHET + MumHET:bAOB + KsHET:I(KsHET * 
+    1) + Kno2HET:o2 + Kno2HET:etaHET + Ko2NOB:no3 + MumNOB:bAOB + 
+    Ko2NOB:factor + MumHET:bEPS + MumAOB:bHET + Ko2HET:nh4 + 
+    bHET:YHET + MumAOB:bAOB + Ko2NOB:EPSratio + Kno3HET:o2 + 
+    KsHET:no3 + Kno2HET:nh4 + bAOB:I(bAOB * 1) + Kno2HET:no3 + 
+    KsHET:Kno2HET + Ko2AOB:YHET + KsHET:MumNOB + Kno2HET:MumNOB + 
+    bNOB:bEPS + Kno2NOB:I(Kno2NOB * 1) + MumHET:time + etaHET:factor + 
+    Knh4AOB:YHET + no3:I(no3 * 1) + bAOB:factor + bNOB:sub + 
+    YHET:I(YHET * 1) + Ko2NOB:nh4 + YEPS:time + KsHET:bAOB + 
+    Ko2HET:etaHET + bNOB:no3 + etaHET:no3 + Knh4AOB:YEPS + Knh4AOB:nh4 + 
+    Knh4AOB:bEPS + Ko2HET:MumAOB + KsHET:MumAOB + no2:I(no2 * 
+    1) + KsHET:factor + Kno2HET:MumAOB + MumAOB:etaHET + Kno2NOB:o2 + 
+    Kno2HET:sub + MumAOB:EPSratio + MumNOB:nh4 + Kno3HET:MumNOB + 
+    MumAOB:o2 + etaHET:bAOB + Ko2HET:bNOB + etaHET:nh4 + Kno3HET:I(Kno3HET * 
+    1) + Ko2HET:bEPS + KsHET:Ko2NOB + Kno2NOB:bHET + Ko2NOB:etaHET + 
+    MumNOB:I(MumNOB * 1) + EPSratio:time + bHET:YEPS + no2:o2 + 
+    Ko2HET:no3 + KsHET:bEPS + bAOB:YHET + factor:time + bEPS:no2 + 
+    Kno3HET:Knh4AOB + Kno2HET:bAOB + Ko2AOB:EPSratio + Ko2HET:EPSratio + 
+    bEPS:no3 + Kno2NOB:MumNOB + YHET:nh4 + Knh4AOB:Ko2NOB + Ko2AOB:no2 + 
+    etaHET:time + no3:time + factor:no3 + MumAOB:factor + Kno3HET:no2 + 
+    Ko2HET:factor + Ko2AOB:etaHET + Kno2HET:I(Kno2HET * 1) + 
+    bEPS:I(bEPS * 1) + o2:time + Ko2AOB:time + MumNOB:time + 
+    KsHET:o2 + Kno2NOB:etaHET + Knh4AOB:no2 + sub:no2 + Kno2HET:EPSratio + 
+    Ko2HET:Knh4AOB + Ko2NOB:bEPS + Ko2HET:o2 + Ko2AOB:Kno2NOB + 
+    MumAOB:YHET + sub:no3 + YEPS:nh4 + o2:nh4 + MumHET:nh4 + 
+    Ko2AOB:o2 + MumNOB:factor + KsHET:sub + no2:nh4 + Ko2AOB:sub + 
+    MumHET:MumAOB + etaHET:no2 + KsHET:nh4 + YEPS:o2 + Kno2HET:YHET + 
+    factor:nh4 + bHET:sub + Kno3HET:nh4 + no3:o2 + bHET:o2 + 
+    Ko2HET:no2 + Ko2HET:Kno2NOB + Kno2NOB:factor + EPSratio:nh4 + 
+    bEPS:YHET
+
+
+c3=~KsHET + Ko2AOB + MumHET + etaHET + bHET + YEPS + YHET + EPSratio + 
+    factor + sub + o2 + time + time:I(time * 1) + etaHET:time + 
+    EPSratio:time + sub:time + YEPS:time + YHET:time + KsHET:time + 
+    etaHET:EPSratio + etaHET:I(etaHET * 1) + EPSratio:I(EPSratio * 
+    1) + EPSratio:sub + etaHET:sub + o2:time + MumHET:sub + factor:time + 
+    Ko2AOB:time + Ko2AOB:MumHET + YEPS:sub + etaHET:YEPS + sub:I(sub * 
+    1) + etaHET:bHET
+
+c4=~KsHET + Kno3HET + etaHET + YEPS + YHET + EPSratio + sub + time + 
+    time:I(time * 1) + EPSratio:time + etaHET:time + etaHET:EPSratio + 
+    sub:time + YEPS:time + EPSratio:I(EPSratio * 1) + YHET:time + 
+    KsHET:time + etaHET:I(etaHET * 1) + EPSratio:sub + etaHET:sub + 
+    Kno3HET:time + etaHET:YEPS
+
+c5=~KsHET + etaHET + YEPS + YHET + EPSratio + sub + o2 + time + 
+    time:I(time * 1) + etaHET:I(etaHET * 1) + EPSratio:time + 
+    EPSratio:I(EPSratio * 1) + o2:time
+#save(form,file=paste(dirname,"form",sep="/"))
