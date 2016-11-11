c627dc2211c6001f98edda3d407428bacc9877d9
shelllbw <libowen96@hotmail.com>
2016-11-11 11:48:39 +0000

    Update nufeb package
    Former-commit-id: 8abd4653e59b8f870ebc81e5bc9a9ff91effee7c
    Former-commit-id: c33149ab1c51c79f7a8f0a72643c7c3e602071cb [formerly ba0355be03200c423233284763384951e66575a0] [formerly a5d0362ec64900db91239ad9c73146754219fbf4 [formerly 6d445b0d20c3b82c1e6b5fd1a076beb058d1ac3b]]
    Former-commit-id: 50267ea0d13388c74576667ae5e90ff782fb7786 [formerly dbf4134a832245d71a6641d311d1ede52e300bbc]
    Former-commit-id: 1d593c2d0c289430979da2f7788d0d9108dfd9cc

---
+++ code/lammps1Feb2014/src/USER-NUFEB/fix_diffusion.h
@@ -0,0 +1,74 @@
+/*
+ * fix_metabolism.h
+ *
+ *  Created on: 15 Aug 2016
+ *      Author: bowen
+ */
+
+#ifdef FIX_CLASS
+
+FixStyle(diffusion,FixDiffusion)
+
+#else
+
+#ifndef SRC_FIX_DIFFUSION_H
+#define SRC_FIX_DIFFUSION_H
+
+#include "fix.h"
+#include <Eigen/Eigen>
+
+using namespace Eigen;
+
+namespace LAMMPS_NS {
+
+class FixDiffusion : public Fix {
+
+ public:
+  FixDiffusion(class LAMMPS *, int, char **);
+  ~FixDiffusion();
+  int setmask();
+  void init();
+  void pre_force(int);
+
+ private:
+  char **var;
+  int *ivar;
+  int nnus;                     // # of nutrients
+
+  int sflag;
+  double **iniS;              // inlet concentrations of nutrients
+  double *diffCoeff;            // diffusion coefficients of nutrients
+  double diffT;                 // diffusion timestamp
+  double tol;                   // tolerance for convergence criteria for nutrient balance equation
+  double ** nuR;
+  double ** nuS;
+  double *r;
+  double* maxBC;
+
+  int nx, ny, nz;               // # of grids in x, y and z
+  int ngrids;                   // total # of grids
+  double xlo,xhi,ylo,yhi,zlo,zhi;
+  int xbcflag, ybcflag, zbcflag;             // 0=PERIODIC-PERIODIC, 1=DIRiCH-DIRICH, 2=NEU-DIRICH, 3=NEU-NEU, 4=DIRICH-NEU
+  double grid;                               // grid height
+  double xbcm, xbcp, ybcm, ybcp, zbcm, zbcp; //inlet BC concentrations for each surface
+  SparseMatrix<double> LAP;       //laplacian matrix
+  SparseMatrix<double> I;       //sparse identity matrix
+
+  class FixKinetics *kinetics;
+  class BIO *bio;
+
+  SparseMatrix<double> laplacian_matrix();
+  void diffusion();
+  SparseMatrix<double> spdiags(MatrixXi&, VectorXi&, int, int, int);
+  VectorXd bc_vec(VectorXd&, double);
+  bool isEuqal(double, double, double);
+
+  void output_data();
+  void test();
+};
+
+}
+
+#endif
+#endif
+
---
+++ code/lammps1Feb2014/src/USER-NUFEB/fix_kinetics_thermo.h
@@ -0,0 +1,66 @@
+/*
+ * fix_metabolism.h
+ *
+ *  Created on: 15 Aug 2016
+ *      Author: bowen
+ */
+
+#ifdef FIX_CLASS
+
+FixStyle(kinetics/thermo,FixKineticsThermo)
+
+#else
+
+#ifndef SRC_FIX_KINETICSTHERMO_H
+#define SRC_FIX_KINETICSTHERMO_H
+
+#include <fix.h>
+
+namespace LAMMPS_NS {
+
+class FixKineticsThermo : public Fix {
+ public:
+  FixKineticsThermo(class LAMMPS *, int, char **);
+  ~FixKineticsThermo();
+  int setmask();
+  void init();
+  void pre_force(int);
+
+ private:
+  char **var;
+  int *ivar;
+
+  int nnus;                     // # of nutrients
+  int ntypes;                   // # of species
+  int nx, ny, nz;               // number of grids in x y z axis
+  int ngrids;                   //# of grids
+
+  double rth, temp;            //Universal gas constant (thermodynamics) and temperature
+
+  double **catCoeff;               // catabolism coefficients of species
+  double **anabCoeff;              // anabolism  coefficients of species
+  double **metCoeff;               //metabolism coefficients of species
+  double **nuGCoeff;
+  double **typeGCoeff;
+  double *yield;                   // yield coefficients
+  double **typeG;                  //type energy for all grids
+  double *diss;                     // Gibbs free energy of dissipation
+  double **iyield;
+
+  double **nuS;                    //nutrient concentration for all grids
+  double **nuG;                    //nutrient energy for all grids
+
+  double **dG0;
+
+  class FixKinetics *kinetics;
+  class BIO *bio;
+
+  void thermo();
+  void init_dG0();
+};
+
+}
+
+#endif
+#endif
+
