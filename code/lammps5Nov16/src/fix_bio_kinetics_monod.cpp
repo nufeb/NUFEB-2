bb763dc2bc5019036cac39fa5e71f2e5124bd093
shelllbw <libowen96@hotmail.com>
2017-05-03 19:01:06 +0100

    Fix bugs

--- code/lammps5Nov16/src/fix_bio_kinetics_monod.cpp
+++ code/lammps5Nov16/src/fix_bio_kinetics_monod.cpp
@@ -331,11 +331,10 @@ double FixKineticsMonod::grid_monod(int pos, int type, int ind)
     double s = nuS[i][pos];
 
     if (ks != 0) {
-      if (s == 1e-20) return 0;
+      //if (s == 1e-20) return 0;
       monod *= s/(ks + s);
     }
   }
-
   return monod;
 }
 
