3530a9c0200cbd9625d3ba3dbc3ae304acdcaf29
Denis Taniguchi <denis.taniguchi@gmail.com>
2018-02-19 13:39:53 -0300

    Making sure AtomVecBio is packing/unpacking outerRadius and outerMass attributes for correct communication. Solves nan problems arising in simulation domain bounaries.
    Former-commit-id: 9e48481d808004eef4e0c1b9683ff61c00cc8edc
    Former-commit-id: 302b8afe48ce3980b2a0385bbd43b2fad94205fc [formerly b08b3547095fbd7573709bb846dc984cc33b172e] [formerly e3b579e0808bbf8859366f172473b2b0b400e974 [formerly b8b4ef5240418bcc82d538c724002b274800100b]]
    Former-commit-id: 516a873a6eaca9bab77316135ee1436d06070667 [formerly df490e68decde80a8e747fb136243edea0ecd8d4]
    Former-commit-id: fbd7765ab1c4ae061ae0c64a23159adf339a1f23

--- code/lammps5Nov16/src/USER-NUFEB/atom_vec_bio.h
+++ code/lammps5Nov16/src/USER-NUFEB/atom_vec_bio.h
@@ -20,38 +20,74 @@ AtomStyle(bio,AtomVecBio)
 #ifndef LMP_ATOM_VEC_BIO_H
 #define LMP_ATOM_VEC_BIO_H
 
-#include "atom_vec_sphere.h"
+#include "atom_vec.h"
 
 namespace LAMMPS_NS {
 
-class AtomVecBio : public AtomVecSphere {
+class AtomVecBio : public AtomVec {
  public:
-
-  //atom
-  double *outerRadius;     //atom outer radius
-  double *outerMass;       //atom outer mass
-  //double *atom_q;         //atom maximum consumption rate
-  int maskEPS;               // mask of EPS group
-  int maskHET;               // mask of HET group
-  int maskDEAD;               // mask of HET group
-  int typeEPS;                // itype of EPS
-  int typeDEAD;                // itype of DEAD
-  //double *virtualMass;
-
-  class BIO *bio;
-
   AtomVecBio(class LAMMPS *);
   ~AtomVecBio();
   void init();
   void grow(int);
-  void create_atom(int itype, double *coord);
-  void data_atom(double *, imageint, char **);
+  void grow_reset();
   void copy(int, int, int);
+  int pack_comm(int, int *, double *, int, int *);
+  int pack_comm_vel(int, int *, double *, int, int *);
+  int pack_comm_hybrid(int, int *, double *);
+  void unpack_comm(int, int, double *);
+  void unpack_comm_vel(int, int, double *);
+  int unpack_comm_hybrid(int, int, double *);
+  int pack_reverse(int, int, double *);
+  int pack_reverse_hybrid(int, int, double *);
+  void unpack_reverse(int, int *, double *);
+  int unpack_reverse_hybrid(int, int *, double *);
+  int pack_border(int, int *, double *, int, int *);
+  int pack_border_vel(int, int *, double *, int, int *);
+  int pack_border_hybrid(int, int *, double *);
+  void unpack_border(int, int, double *);
+  void unpack_border_vel(int, int, double *);
+  int unpack_border_hybrid(int, int, double *);
+  int pack_exchange(int, double *);
+  int unpack_exchange(double *);
+  int size_restart();
+  int pack_restart(int, double *);
+  int unpack_restart(double *);
+  void create_atom(int, double *);
+  void data_atom(double *, imageint, char **);
+  int data_atom_hybrid(int, char **);
+  void data_vel(int, char **);
+  int data_vel_hybrid(int, char **);
+  void pack_data(double **);
+  int pack_data_hybrid(int, double *);
+  void write_data(FILE *, int, double **);
+  int write_data_hybrid(FILE *, double *);
+  void pack_vel(double **);
+  int pack_vel_hybrid(int, double *);
+  void write_vel(FILE *, int, double **);
+  int write_vel_hybrid(FILE *, double *);
   bigint memory_usage();
 
- private:
-  void set_group_mask();
+  double *outerRadius;     // atom outer radius
+  double *outerMass;       // atom outer mass
+  int maskEPS;             // mask of EPS group
+  int maskHET;             // mask of HET group
+  int maskDEAD;            // mask of HET group
+  int typeEPS;             // itype of EPS
+  int typeDEAD;            // itype of DEAD
 
+  class BIO *bio;
+
+ private:
+  tagint *tag;
+  int *type,*mask;
+  imageint *image;
+  double **x,**v,**f;
+  double *radius,*rmass;
+  double **omega,**torque;
+  int radvary;
+
+  void set_group_mask();
 };
 
 }
