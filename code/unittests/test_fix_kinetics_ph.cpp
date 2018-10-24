6eda82dc452da3a3f5b0777c7dd662be68ee441c
shelllbw <libowen96@hotmail.com>
2018-10-24 12:16:37 +0100

    Update unittests; Add nufeb and cfddem package to lammps MAKE

--- code/unittests/test_fix_kinetics_ph.cpp
+++ code/unittests/test_fix_kinetics_ph.cpp
@@ -1,11 +1,11 @@
 /*
- * test_fix_division.cpp
+ * test_fix_kinetics_ph.cpp
  *
- *  Created on: 3 Dec 2016
- *      Author: bowen
+ *  Created on: 22 Oct 2018
+ *      Author: Bowen Li
  */
 
-#include "test_fix_kinetics_monod.h"
+#include "test_fix_kinetics_ph.h"
 
 using namespace std;
 
@@ -14,19 +14,18 @@ using namespace LAMMPS_NS;
 namespace {
 
 // The fixture for testing class Foo.
-  class FixKineticsMonodTest : public ::testing::Test {
+  class FixKineticsPHTest : public ::testing::Test {
   protected:
-    FixKineticsMonodTest() {
+    FixKineticsPHTest() {
       kinetics = NULL;
-      avec = NULL;
 
-      char *argv[] = {"fix_kinetics_monod", "-screen", "none", "-log", "none", NULL};
+      char *argv[] = {"fix_kinetics_ph", "-screen", "none", "-log", "none", NULL};
       int narg = 5;
       // Create a LAMMPS Object
       lmp = new LAMMPS(narg,argv,MPI_COMM_WORLD);
     }
 
-    virtual ~FixKineticsMonodTest(){
+    virtual ~FixKineticsPHTest(){
       lmp->destroy();
     }
 
@@ -34,17 +33,20 @@ namespace {
       // Set atom_style
       lmp->input->one("atom_style bio");
       // Modify atom_style parameters
-      lmp->input->one("atom_modify map array sort 1000 5.0e-7");
-      lmp->input->one("read_data_bio inputs/monod.in");
+      lmp->input->one("atom_modify map array sort 100 5.0e-7");
+      lmp->input->one("read_data_bio inputs/ph.in");
       // Create group
-      lmp->input->one("group HET type 1");
+      lmp->input->one("group AOB type 1");
       // Define variables
-      lmp->input->one("variable temp equal 298.15");
-      lmp->input->one("variable gasTran equal 0.08205746");
-      lmp->input->one("variable gvol equal 2e-11");
+      lmp->input->one("variable diffT equal 1e-4");
+      lmp->input->one("variable layer equal -1");
       lmp->input->one("variable EPSdens equal 30");
-
-      avec = (AtomVecBio *) lmp->atom->style_match("bio");
+      // set timestep and fix
+      lmp->input->one("timestep 3600");
+      lmp->input->one("fix k1 all kinetics 1 2 2 2 v_diffT v_layer");
+      lmp->input->one("fix ki3 all kinetics/ph");
+      lmp->input->one("fix ki2 all kinetics/thermo");
+      lmp->input->one("fix ki1 all kinetics/growth/energy v_EPSdens");
     }
 
     virtual void TearDown() {
@@ -53,53 +55,25 @@ namespace {
 
     // Objects declared here can be used by all tests in the test case for Foo.
     LAMMPS* lmp;
-    AtomVecBio *avec;
     FixKinetics *kinetics;
   };
 
   /*
-   *  Check for new mass/radius after non-diffusion growth
+   *  Check sh
    */
- TEST_F(FixKineticsMonodTest, No_Diffusion_1step) {
-   lmp->input->one("timestep 36000");
-   lmp->input->one("fix k1 all kinetics 1 1 1 v_temp");
-   lmp->input->one("fix ki1 all kinetics/monod 1 0 v_gasTran v_gvol v_EPSdens");
-
+ TEST_F(FixKineticsPHTest, Var_SH) {
    int ifix = lmp->modify->find_fix("k1");
+
    if (ifix >= 0) {
      kinetics = static_cast<FixKinetics *>(lmp->modify->fix[ifix]);
    }
+
    // run lammps
    lmp->input->one("run 1");
-
-   EXPECT_NEAR(3.02e-17, lmp->atom->rmass[0], 1e-19);
-   EXPECT_NEAR(3.63e-07, lmp->atom->radius[0], 1e-9);
-   EXPECT_NEAR(7.68e-17, avec->outerMass[0], 1e-19);
-   EXPECT_NEAR(8.70e-07, avec->outerRadius[0], 1e-9);
- }
-
- /*
-  *  Check for new mass/radius after diffusion growth
-  */
- TEST_F(FixKineticsMonodTest, Diffusion_1step_Consumption) {
-   TearDown();
-   SetUp();
-   lmp->input->one("variable diffT equal 1e-3");
-   lmp->input->one("variable tol equal 1e-6");
-
-   lmp->input->one("timestep 36000");
-   lmp->input->one("fix k1 all kinetics 1 1 1 v_temp");
-   lmp->input->one("fix g1 all diffusion exp v_diffT v_tol pp pp nd");
-   lmp->input->one("fix km1 all kinetics/monod 1 1 v_gasTran v_gvol v_EPSdens");
-
-   int ifix = lmp->modify->find_fix("k1");
-   if (ifix > -1) {
-     kinetics = static_cast<FixKinetics *>(lmp->modify->fix[ifix]);
+   // loop over all grids which should have the same value
+   for (int i = 0; i < 8; i++) {
+     EXPECT_NEAR(1.020407e-10, kinetics->sh[i], 1e-16);
    }
-   // run lammps
-   lmp->input->one("run 1");
-
-   EXPECT_NEAR(-9.01e-10, kinetics->nuR[1][0], 1e-12);
  }
 }
 
