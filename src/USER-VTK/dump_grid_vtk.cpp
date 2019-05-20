a0cb8f42e78f51b2b4ab34433014257fe2ff80da
Denis Taniguchi <denis.taniguchi@ncl.ac.uk>
2019-05-20 11:25:49 +0100

    Renaming dump_grid_vtk files and adding to gitignore.

--- src/USER-VTK/dump_grid_vtk.cpp
+++ src/USER-VTK/dump_grid_vtk.cpp
@@ -11,7 +11,7 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
-#include "dump_grid.h"
+#include "dump_grid_vtk.h"
 
 #include "atom.h"
 #include "comm.h"
