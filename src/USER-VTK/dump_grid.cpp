5599bae64717b1a3e18c1ae1ea24ff94d4272273
Denis Taniguchi <denis.taniguchi@ncl.ac.uk>
2019-05-20 11:21:19 +0100

    Adding dump grid/vtk.
    Former-commit-id: 088018b4fff8a7260833cac6636a9f764075da63

---
+++ src/USER-VTK/dump_grid.cpp
@@ -0,0 +1,165 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#include "dump_grid.h"
+
+#include "atom.h"
+#include "comm.h"
+#include "error.h"
+#include "modify.h"
+#include "update.h"
+#include "grid.h"
+#include "grid_masks.h"
+#include "domain.h"
+
+#include <vtkCellData.h>
+#include <vtkDataArray.h>
+#include <vtkDoubleArray.h>
+#include <vtkXMLImageDataWriter.h>
+
+#include <regex>
+#include <sstream>
+
+using namespace LAMMPS_NS;
+
+DumpGridVTK::DumpGridVTK(LAMMPS *lmp, int narg, char **arg) : Dump(lmp, narg, arg)
+{
+  filewriter = 0;
+  parse_fields(narg, arg);
+}
+
+DumpGridVTK::~DumpGridVTK() {}
+
+void DumpGridVTK::init_style() {
+  using std::placeholders::_1;
+
+  if (!grid)
+    error->all(FLERR, "No grid defined for dump grid/vtk");
+  
+  for (auto it = fields.begin(); it != fields.end(); ++it) {
+    if (*it == "con") {
+      packs.push_back(std::bind(&DumpGridVTK::pack_concentration, this, _1));
+    }
+    else if (*it == "rea") {
+      packs.push_back(std::bind(&DumpGridVTK::pack_reaction, this, _1));
+    }
+  }
+}
+
+void DumpGridVTK::write() {
+  vtkSmartPointer<vtkImageData> image = vtkSmartPointer<vtkImageData>::New();
+  image->SetDimensions(grid->subbox[0] - 1, grid->subbox[1] - 1, grid->subbox[2] - 1);
+  image->SetSpacing(grid->cell_size, grid->cell_size, grid->cell_size);
+  double origin[3];
+  for (int i = 0; i < 3; i++)
+    origin[i] = grid->cell_size * (grid->sublo[i] + 1) - domain->boxlo[i];
+  image->SetOrigin(origin[0], origin[1], origin[2]);
+
+  for (auto it = packs.begin(); it != packs.end(); ++it) {
+    (*it)(image);
+  }
+
+  // filename must contain '%' and '*'
+  std::string str(filename);
+  auto perc = str.find('%');
+  if (perc == std::string::npos) {
+    error->all(FLERR, "dump grid filename must contain '%' special character");
+  }
+  auto star = str.find('*');
+  if (star == std::string::npos) {
+    error->all(FLERR, "dump grid filename must contain '*' special character");
+  }
+
+  str = std::regex_replace(str, std::regex("%"), std::to_string(comm->me));
+  str = std::regex_replace(str, std::regex("\\*"), std::to_string(update->ntimestep));
+
+  vtkSmartPointer<vtkXMLImageDataWriter> writer = vtkSmartPointer<vtkXMLImageDataWriter>::New();
+  writer->SetFileName(str.c_str());
+  writer->SetInputData(image);
+  writer->Write();
+}
+
+int DumpGridVTK::parse_fields(int narg, char **arg) {
+  int i;
+  for (int iarg = 0; iarg < narg; iarg++) {
+    i = iarg;
+    if (strcmp(arg[iarg],"con") == 0) {
+      fields.push_back(arg[iarg]);
+    } else if (strcmp(arg[iarg], "upt") == 0) {
+      fields.push_back(arg[iarg]);
+    } else if (strcmp(arg[iarg], "act") == 0) {
+      fields.push_back(arg[iarg]);
+    } else if (strcmp(arg[iarg], "yie") == 0) {
+      fields.push_back(arg[iarg]);
+    } else if (strcmp(arg[iarg], "cat") == 0) {
+      fields.push_back(arg[iarg]);
+    } else if (strcmp(arg[iarg], "ana") == 0) {
+      fields.push_back(arg[iarg]);
+    } else if (strcmp(arg[iarg], "hyd") == 0) {
+      fields.push_back(arg[iarg]);
+    }
+  }
+}
+
+void DumpGridVTK::pack_concentration(vtkSmartPointer<vtkImageData> image) {
+  pack_tuple1(image, "concentration", grid->conc, grid->sub_names, grid->nsubs);
+}
+
+void DumpGridVTK::pack_reaction(vtkSmartPointer<vtkImageData> image) {
+  pack_tuple1(image, "reaction", grid->reac, grid->sub_names, grid->nsubs);
+}
+
+void DumpGridVTK::pack_tuple1(vtkSmartPointer<vtkImageData> image, const char *name, double *data) {
+  vtkSmartPointer<vtkDoubleArray> array = vtkSmartPointer<vtkDoubleArray>::New();
+  array->SetName(name);
+  array->SetNumberOfComponents(1);
+  for (int i = 0; i < grid->ncells; i++) {
+    if (!(grid->mask[i] & GHOST_MASK))
+      array->InsertNextTuple1(data[i]);
+  }
+  image->GetCellData()->AddArray(array);
+}
+
+void DumpGridVTK::pack_tuple1(vtkSmartPointer<vtkImageData> image, const char *name, double **data, char **names, int count) {
+  for (int n = 0; n < count; n++) {
+    vtkSmartPointer<vtkDoubleArray> array = vtkSmartPointer<vtkDoubleArray>::New();
+    std::ostringstream oss;
+    oss << names[n] << " " << name;
+    array->SetName(oss.str().c_str());
+    array->SetNumberOfComponents(1);
+    for (int i = 0; i < grid->ncells; i++) {
+      if (!(grid->mask[i] & GHOST_MASK))
+	array->InsertNextTuple1(data[n][i]);
+    }
+    image->GetCellData()->AddArray(array);
+  }
+}
+
+void DumpGridVTK::pack_tuple5(vtkSmartPointer<vtkImageData> image, const char *name, double ***data, char **names, int count) {
+  for (int n = 0; n < count; n++) {
+    vtkSmartPointer<vtkDoubleArray> array = vtkSmartPointer<vtkDoubleArray>::New();
+    std::ostringstream oss;
+    oss << names[n] << " " << name;
+    array->SetName(oss.str().c_str());
+    array->SetNumberOfComponents(5);
+    for (int i = 0; i < grid->ncells; i++) {
+      if (!(grid->mask[i] & GHOST_MASK)) {
+	double tuple[5];
+	for (int j = 0; j < 5; j++)
+	  tuple[j] = data[n][j][i];
+	array->InsertNextTuple(tuple);
+      }
+    }
+    image->GetCellData()->AddArray(array);
+  }
+}
