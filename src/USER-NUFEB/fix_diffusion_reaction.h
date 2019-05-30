a19ce75b4f7f486d333e59e7cca9e94ccca91600
Denis Taniguchi <denis.taniguchi@ncl.ac.uk>
2019-05-30 10:30:44 +0100

    Adding fix to compute diffusion reaction.
    Former-commit-id: a2c34dbb4826388e9b300fa9ea07bf7c2516c88b

---
+++ src/USER-NUFEB/fix_diffusion_reaction.h
@@ -0,0 +1,53 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef FIX_CLASS
+
+FixStyle(nufeb/diffusion_reaction,FixDiffusionReaction)
+
+#else
+
+#ifndef LMP_FIX_DIFFUSION_REACTION_H
+#define LMP_FIX_DIFFUSION_REACTION_H
+
+#include "fix.h"
+
+namespace LAMMPS_NS {
+
+class FixDiffusionReaction : public Fix {
+ public:
+  bool compute_flag;
+
+  FixDiffusionReaction(class LAMMPS *, int, char **);
+  virtual ~FixDiffusionReaction();
+  int setmask();
+  int modify_param(int, char **);
+  virtual void init();
+  virtual void initial_integrate(int);
+  virtual void final_integrate();
+  virtual double compute_scalar();
+
+ protected:
+  int isub;
+  double diff_coef;
+  int ncells;
+  double *prev;
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+*/
