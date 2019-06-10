bbae8c551cdfc90b38b4569cde6eb448d1f200f2
Denis Taniguchi <denis.taniguchi@ncl.ac.uk>
2019-06-10 11:04:40 +0100

    Wrong type on FixDiffusionReaction MPI_Allreduce call.

--- src/USER-NUFEB/fix_diffusion_reaction.cpp
+++ src/USER-NUFEB/fix_diffusion_reaction.cpp
@@ -163,6 +163,6 @@ double FixDiffusionReaction::compute_scalar()
       result = MAX(result, fabs((grid->conc[isub][i] - prev[i]) / prev[i]));
     }
   }
-  MPI_Allreduce(MPI_IN_PLACE, &result, 1, MPI_INT, MPI_MAX, world);
+  MPI_Allreduce(MPI_IN_PLACE, &result, 1, MPI_DOUBLE, MPI_MAX, world);
   return result;
 }
