a5170e4d23fc42aa50a5cc4c56df90f48529aac9
shelllbw <libowen96@hotmail.com>
2021-04-30 13:04:05 +0100

    Add the fourth boundary condition 'bulk(b)' to fix_diffusion_reaction
    Former-commit-id: 8adb38b90d2b5d4e0ccfacbdc39b1e45b437964d
    Former-commit-id: 0e7b0516e5742a2d2424a13b94f9b792a93fc070 [formerly 5ba9d911ff3c1d0d44ce7e7e7b55ef8674fdc8a6] [formerly 6d069ba532729af2523bdea320bb8f44ef0698c3 [formerly 65de43f8e744ef2cacf96b43be5059c250445fe7]] [formerly f7433de6e25b4f21a492a9f4950c30705c725d0e [formerly 14d18f3eb725769b718eac51c9ce4e67ab9ada72] [formerly 4994ece82a12b3d172e07fbef503e5218713b9ca [formerly bffa7f248e5f69463cc4f5c9a905072be4e9bbaa]]]
    Former-commit-id: 3e110516e1dc657a9210327667bef459df97d8c8 [formerly 5a7524d51254df8d1da0e7e59623aec338e750b7] [formerly 51ed9079233f40264019018c247c643bce394360 [formerly 8546d7c57f63ad615dd80546d122624588abe841]]
    Former-commit-id: d6e29c6b062bd8ed1e1a1804d957bb5399920ec7 [formerly d8a948bfb59487f964f7ac264f417d86dbe12444]
    Former-commit-id: 2c0334513abcf0497e6dd9fa98fcc46d4d59a5f3

--- src/USER-NUFEB/fix_diffusion_reaction.cpp
+++ src/USER-NUFEB/fix_diffusion_reaction.cpp
@@ -28,7 +28,7 @@
 using namespace LAMMPS_NS;
 using namespace FixConst;
 
-enum{DIRICHLET,NEUMANN,PERIODIC};
+enum{DIRICHLET,NEUMANN,PERIODIC,BULK};
 
 /* ---------------------------------------------------------------------- */
 
@@ -60,28 +60,31 @@ FixDiffusionReaction::FixDiffusionReaction(LAMMPS *lmp, int narg, char **arg) :
   diff_coef = force->numeric(FLERR, arg[4]);
 
   for (int i = 0; i < 3; i++) {
-    if (strcmp(arg[5+i], "dd") == 0) {
-      boundary[2*i] = DIRICHLET;
-      boundary[2*i+1] = DIRICHLET;
-      ndirichlet += 2;
-    } else if (strcmp(arg[5+i], "dn") == 0) {
-      boundary[2*i] = DIRICHLET;
-      boundary[2*i+1] = NEUMANN;
-      ndirichlet += 1;
-    } else if (strcmp(arg[5+i], "nd") == 0) {
-      boundary[2*i] = NEUMANN;
-      boundary[2*i+1] = DIRICHLET;
-      ndirichlet += 1;
-    } else if (strcmp(arg[5+i], "nn") == 0) {
-      boundary[2*i] = NEUMANN;
-      boundary[2*i+1] = NEUMANN;
-    } else if (strcmp(arg[5+i], "pp") == 0) {
-      boundary[2*i] = PERIODIC;
-      boundary[2*i+1] = PERIODIC;
-      grid->periodic[i] = 1;
-    } else {
+    if ((arg[5+i][0] == 'p' && arg[5+i][1] != 'p') ||
+	(arg[5+i][1] == 'p' && arg[5+i][0] != 'p'))
       error->all(FLERR, "Illegal boundary condition");
+
+    for (int j = 0; j < 2; j++) {
+      if (arg[5+i][j] == 'p') {
+	boundary[2*i+j] = PERIODIC;
+	grid->periodic[i] = 1;
+      } else if (arg[5+i][j] == 'n') {
+	boundary[2*i+j] = NEUMANN;
+      } else if (arg[5+i][j] == 'd') {
+	boundary[2*i+j] = DIRICHLET;
+	ndirichlet++;
+      } else if (arg[5+i][j] == 'b') {
+	if (!grid->reactor_flag)
+	  error->all(FLERR, "Illegal boundary condition");
+	boundary[2*i+j] = BULK;
+      } else {
+	error->all(FLERR, "Illegal boundary condition");
+      }
     }
+  }
+
+  for (int i = 0; i<6;i++) {
+    printf("i=%i b=%i \n", i, boundary[i]);
   }
 
   if (!ndirichlet) closed_system = 1;
@@ -220,6 +223,18 @@ void FixDiffusionReaction::compute_initial()
       grid->conc[isub][i] = dirichlet[4];
     } else if (grid->mask[i] & Z_PB_MASK && boundary[5] == DIRICHLET) {
       grid->conc[isub][i] = dirichlet[5];
+    } else if (grid->mask[i] & X_NB_MASK && boundary[0] == BULK) {
+      grid->conc[isub][i] = grid->bulk[isub];
+    } else if (grid->mask[i] & X_PB_MASK && boundary[1] == BULK) {
+      grid->conc[isub][i] = grid->bulk[isub];
+    } else if (grid->mask[i] & Y_NB_MASK && boundary[2] == BULK) {
+      grid->conc[isub][i] = grid->bulk[isub];
+    } else if (grid->mask[i] & Y_PB_MASK && boundary[3] == BULK) {
+      grid->conc[isub][i] = grid->bulk[isub];
+    } else if (grid->mask[i] & Z_NB_MASK && boundary[4] == BULK) {
+      grid->conc[isub][i] = grid->bulk[isub];
+    } else if (grid->mask[i] & Z_PB_MASK && boundary[5] == BULK) {
+      grid->conc[isub][i] = grid->bulk[isub];
     }
     grid->reac[isub][i] = 0.0;
   }
