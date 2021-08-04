d72e19f8861a7ff4325890a6da0540cfc9d95ff0
shelllbw <libowen96@hotmail.com>
2021-08-04 20:58:57 +0100

    Add compute_ave_length to USER-NUFEB
    Former-commit-id: 2707937129846eebeb6e1197fe43e6a7ea944da0
    Former-commit-id: a3543875988ef7aaad2bdb64adc86adb5e6eeffe [formerly 01fc3fed771abe483ed43f06d8c86f6e25f7afe6] [formerly fb8d88dac11431317f276f1115dea76592b449bd [formerly 5a3daa39f5b7950b261a0ab888fc451f2a850ada]] [formerly 9c6656428173108674b383f8fe6d497a9956fe04 [formerly 60771c4a7d3b806a322ee3698489add6aa6ab42f] [formerly b335de54f953100f56a3375a402f6a57e662741e [formerly 27e636160a0facfe14275d1b066a5d7062d3631a]]]
    Former-commit-id: 7c961c38927a4056d90e5c436c0e02038cd09c33 [formerly 20126126c1d3b1090c0022b2d0aad741d73e0038] [formerly 4f55ee42388530db76c1db05c7dd18ba0c494dc1 [formerly 1f361e9753b82f0e632297a1ffd90e0345eec68b]]
    Former-commit-id: 6f47705c79c12e12fd94ad7e2d463b8c9d8a33c9 [formerly 3e6687881c0c8e4d81369093f2790380bd961568]
    Former-commit-id: e01e7aa84c521cd1fd94f1f70d1626c81f221610

---
+++ src/USER-NUFEB/compute_ave_length.h
@@ -0,0 +1,43 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   http://lammps.sandia.gov, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+#ifdef COMPUTE_CLASS
+
+ComputeStyle(nufeb/ave_length,ComputeAveLength)
+
+#else
+
+#ifndef LMP_COMPUTE_AVE_LENGTH_H
+#define LMP_COMPUTE_AVE_LENGTH_H
+
+#include "compute.h"
+
+namespace LAMMPS_NS {
+
+class ComputeAveLength : public Compute {
+ public:
+  ComputeAveLength(class LAMMPS *, int, char **);
+  virtual ~ComputeAveLength() {}
+  virtual void init() {}
+  virtual double compute_scalar();
+
+  class AtomVecBacillus *avec;
+};
+
+}
+
+#endif
+#endif
+
+/* ERROR/WARNING messages:
+*/
