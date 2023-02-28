bd5b3a27a4a5f76e9f5684bf930c2382cd81e7ba
Bowen Li <libowen96@hotmail.com>
2023-02-28 13:40:28 +0000

    Correct typo

--- src/USER-NUFEB/fix_adhesion_eps.cpp
+++ src/USER-NUFEB/fix_adhesion_eps.cpp
@@ -78,10 +78,11 @@ int FixEPSAdhesion::setmask()
 /* ---------------------------------------------------------------------- */
 
 void FixEPSAdhesion::init() {
-  int irequest = neighbor->request(this,instance_me);
+  int irequest = neighbor->request(this, instance_me);
   neighbor->requests[irequest]->pair = 0;
   neighbor->requests[irequest]->fix = 1;
   neighbor->requests[irequest]->size = 1;
+}
 
 /* ---------------------------------------------------------------------- */
 
