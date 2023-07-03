24ae46292009ba210da539401b92ff90c010dae4
Bowen Li <libowen96@hotmail.com>
2023-07-03 22:39:14 +0100

    Update sedifoam installation script

--- thirdparty/install-sedifoam.sh
+++ thirdparty/install-sedifoam.sh
@@ -8,7 +8,7 @@ cd ${0%/*} || exit 1 # Run from this directory
 
 # Read the information of current directory.
 # And collect information of the installation of LAMMPS from user.
-echo "Installing SediFOAM (for mac/linux).."
+echo "Installing sediFoam (for mac/linux).."
 
 echo "*******************************************"
 echo "select the system you are running, then press enter"
@@ -24,7 +24,6 @@ case $n in
   2) echo "You chose 2) Ubuntu17.x - Ubuntu20.x";;
   3) echo "You chose 3) Centos";;
   4) echo "You chose 4) Mac";;
-  5) echo "You chose 5) Other";;
   *) echo "Unknown option"; exit;;
 esac
 
@@ -64,34 +63,29 @@ make yes-NUFEB
 make yes-COLLOID
 make yes-SEDIFOAM
 
+# Build NUFEB dynamic library
+make -j4 shanghaimac mode=shlib
+cd $FOAM_LIBBIN || exit 1
+    
 # Use different options according to different versions
-if [ $n == 4 ]
-then 
-    make -j4 shanghaimac mode=shlib
-    cd $FOAM_LIBBIN || exit 1
+if [ $n == 4 ];then 
     ln -sf $lammps_dir/src/liblammps_shanghaimac.so .
     cd $sedifoam_dir/lammpsFoam || exit 1
     touch Make/options
     echo "LAMMPS_DIR ="$lammps_src > Make/options
     cat Make/options-mac-openmpi >> Make/options
 else
-    make -j4 shanghailinux mode=shlib
-    cd $FOAM_LIBBIN || exit 1
     ln -sf $lammps_dir/src/liblammps_shanghailinux.so .
     cd $sedifoam_dir/lammpsFoam || exit 1
     touch Make/options
     echo "LAMMPS_DIR ="$lammps_src > Make/options
 
-    if [ $n == 1 ] 
-    then 
-	cat Make/options-ubuntu16-openmpi >> Make/options
-    elif [ $n == 2 ] 
-    then 
-	cat Make/options-ubuntu18-openmpi >> Make/options
-    elif [ $n == 3 ] 
-    then 
-	cat Make/options-linux-openmpi >> Make/options
-    fi
+    case $n in
+	1) cat Make/options-ubuntu16-openmpi >> Make/options ;;
+	2) cat Make/options-ubuntu18-openmpi >> Make/options ;;
+	3) cat Make/options-linux-openmpi >> Make/options ;;
+    esac
+
 fi 
 
 wmake libso dragModels 
